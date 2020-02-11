4867d7a48d536f1acd00f88fae5aac13a958664e
minh <hoangminh1612@gmail.com>
2020-02-10 19:47:26 -0800

    add and test transfer function

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,13 +2,46 @@ pragma solidity ^0.5.16;
 
 /**
  * The DappToken contract does this and that...
+ * Following the standard at
+ * https://eips.ethereum.org/EIPS/eip-20
  */
 contract DappToken {
 
-	uint public totalSupply;
+	string public name = 'Minh Token';
+	string public symbol = 'MiT';
+	string public standard = 'Minh Token v1.0';
+	uint256 public totalSupply;
+
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint256 _value
+		);
+
+	mapping (address => uint256) public balanceOf;
+	
+
 	// Constructor
-  	constructor() public {
-    	totalSupply = 1000000;
+  	constructor(uint256 _initialSupply) public {
+
+  		balanceOf[msg.sender] = _initialSupply;
+    	totalSupply = _initialSupply;
   	}
+
+  	// Transfer
+  	function transfer(address _to, uint256 _value) public returns(bool success) {
+  		// Check the account have enough token
+  		require(balanceOf[msg.sender] >= _value);
+
+  		balanceOf[msg.sender] -= _value;
+  		balanceOf[_to] = _value;
+
+  		// Trigger the event
+  		emit Transfer(msg.sender, _to, _value);
+
+  		return true;
+  		
+  	}
+  	
 
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -7,8 +7,57 @@ contract("DappToken", (accounts) => {
 		dappToken = await DappToken.deployed();
 	});
 
+	it('initializes the contract with the correct value', async () => {
+		var name = await dappToken.name();
+		assert.equal(name, 'Minh Token', 'has the correct name');
+
+		var symbol = await dappToken.symbol();
+		assert.equal(symbol, 'MiT', 'has the correct symbol');
+
+		var standard = await dappToken.standard();
+		assert.equal(standard, 'Minh Token v1.0', 'has the correct standard');
+	});
+
 	it('sets the total supply upon deployment', async () => {
 		var totalSupply = await dappToken.totalSupply();
 		assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
+
+		var adminBalance = await dappToken.balanceOf(accounts[0]);
+		assert.equal(adminBalance.toNumber(), 1000000, 'allocates the initial supply to the admin account');
 	});
+
+	it('transfer token ownership', async () => {
+		var total = 1000000;
+		var amount = 250000;
+		try {
+        	await dappToken.transfer(accounts[1], 999999999);
+        	assert.fail();
+      	} catch(error) {
+        	assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+      	}
+
+      	var success;
+      	try {
+      		success = await dappToken.transfer.call(accounts[1], amount, { from: accounts[0] });
+      	} catch(error) {
+      		success = false;
+      	} finally {
+      		assert.equal(success, true, 'should return true');
+      	}
+      	
+
+      	var result = await dappToken.transfer(accounts[1], amount, { from: accounts[0] });
+
+      	assert.equal(result.logs.length, 1, 'triggers one event');
+      	assert.equal(result.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+      	assert.equal(result.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
+      	assert.equal(result.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
+      	assert.equal(result.logs[0].args._value, amount, 'logs the transfer amount');
+
+      	var balance = await dappToken.balanceOf(accounts[1]);
+      	assert.equal(balance.toNumber(), amount, 'adds the amount to the receiving account');
+      	balance = await dappToken.balanceOf(accounts[0]);
+      	assert.equal(balance.toNumber(), total - amount, 'deduct the amount from the sending account');
+
+	})
 });
